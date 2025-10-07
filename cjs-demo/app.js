b45e8ea5f814ba189e9b7c027b97ebab38b1d8d0
Gadhe-Sai-prasad <154505332+Gadhe-Sai-prasad@users.noreply.github.com>
2025-10-07 14:24:47 +0530

    Add files via upload

---
+++ cjs-demo/app.js
@@ -0,0 +1,8 @@
+const {add,multiply} =require('./math');
+const {greet} = require('./greet');
+
+
+ 
+console.log(greet('Prudhvi'));
+console.log('Sum: ' + add(5, 3));
+console.log('Product: ' + multiply(5, 3));
