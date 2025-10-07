34c0636862facaa134204bda274b808bbf0dbe3e
Gadhe-Sai-prasad <154505332+Gadhe-Sai-prasad@users.noreply.github.com>
2025-10-07 14:27:26 +0530

    Add files via upload

---
+++ esm-demo/app.mjs
@@ -0,0 +1,6 @@
+import {greet} from "./greet.mjs"
+import {add,multiply} from "./math.mjs"
+
+console.log(greet("sai"));
+console.log('sum  '   + add(5,6));
+console.log('product  '   + multiply(7,8));
